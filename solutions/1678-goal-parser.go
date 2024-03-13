0feb734d3017bda2da35c06f3610ca1de2c343f3
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-03-12 23:28:24 -0300

    1678

---
+++ solutions/1678-goal-parser.go
@@ -0,0 +1,58 @@
+package solutions
+
+func interpret(command string) string {
+	var outputText string
+
+	var hashMap = map[string]string{
+		"G":    "G",
+		"(al)": "al",
+		"()":   "o",
+	}
+
+	var word string
+
+	for _, v := range command {
+		if item, ok := hashMap[word]; ok {
+			outputText = item
+			word = ""
+		}
+	}
+
+	return outputText
+
+}
+
+/*
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+
+
+Constraints:
+
+1 <= command.length <= 100
+command consists of "G", "()", and/or "(al)" in some order.
+
+
+*/
